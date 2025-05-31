7d04d8f1ba6243365df4c68c0466e8983848ca64
Thomas Lenz <Thomas.Lenz85@gmail.com>
2025-05-31 18:52:24 +0200

    Add interfaces from task

---
+++ MeasurementDataBuffer/MeasurementDataBuffer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasurementDataBuffer
+{
+    public interface IMeasurementData
+    {
+        // Example properties not mentioned in the task
+        DateTime Timestamp { get; }
+        double Value { get; }
+    }
+
+    public interface IDao
+    {
+        bool SaveMeasurementData(IMeasurementData measurement);
+    }
+
+    internal class MeasurementDataBuffer
+    {
+    }
+}
